2bba5a1fd835fa9894e1341d519f6de4f6e3a5a1
Romain <w.shark@hotmail.fr>
2014-03-17 10:06:43 +0100

    Ajout de la page pour lire les mailings

--- app/controller/notificationsController.php
+++ app/controller/notificationsController.php
@@ -1,6 +1,23 @@
 <?php
 
 class notificationsController extends Controller{
+
+	public function readAction($nid){
+		// Recuperation dans la base
+		$data = $this->registry->db->get_one('notifications', array('id =' => $nid));
+		
+		// Verification utilisateur peut voir la notification
+		if(empty($data)){return 'Erreur : la notification recherchée n\'existe pas !';}
+		if($data['user_id'] != $_SESSION['utilisateur']['id']){return 'Erreur : requete invalide !';}
+		
+		// On marque la notification comme lu
+		$data['is_read'] = 1;
+		$this->registry->db->update('notifications', $data, array('id =' => $nid));
+		
+		$this->registry->smarty->assign('notification', $data);
+		
+		return $this->registry->smarty->fetch(VIEW_PATH . 'notifications' . DS . 'read.tpl');
+	}
 	
 	public function getcountAction($user_id){
 		$nb = $this->registry->db->count('notifications', array('user_id =' => $user_id, 'is_read =' => 0, 'is_delete =' => 0));
---
+++ app/view/notifications/read.tpl
@@ -0,0 +1,11 @@
+<h3>Notification #{$notification.id}</h3>
+<table class="table">
+	<tr>
+		<td>Date :</td>
+		<td>{$notification.date_notification}</td>
+	</tr>
+	<tr>
+		<td>Notification :</td>
+		<td>{$notification.message}</td>
+	</tr>
+</table>
